c8f39b651ad8bd4bcff186e405c5f5f4027c5434
Ebin Soby <mailebinsoby@gmail.com>
2024-04-24 00:47:50 -0400

    Updates

---
+++ precache-manifest.cabf2a312186022193bc9ecd7a4ac6a6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a47a5e729241e6b8e168660f0b8b104",
+    "url": "/Portfolio-1.0/index.html"
+  },
+  {
+    "revision": "d550322deed4c1e7deb9",
+    "url": "/Portfolio-1.0/static/css/main.ad482c5d.chunk.css"
+  },
+  {
+    "revision": "0d6f428102c00c08a640",
+    "url": "/Portfolio-1.0/static/js/2.85d2db75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-1.0/static/js/2.85d2db75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d550322deed4c1e7deb9",
+    "url": "/Portfolio-1.0/static/js/main.cc003478.chunk.js"
+  },
+  {
+    "revision": "21beb94cb2048b3ba24f",
+    "url": "/Portfolio-1.0/static/js/runtime-main.f5e0f93e.js"
+  },
+  {
+    "revision": "b22aa71b5637ce1a6dec495f9167ec3b",
+    "url": "/Portfolio-1.0/static/media/Ana_B&W.b22aa71b.png"
+  },
+  {
+    "revision": "56e9f36cc011b5a2f009cb8986bf2360",
+    "url": "/Portfolio-1.0/static/media/Distanceware.56e9f36c.png"
+  },
+  {
+    "revision": "938d449c66648cbab2dcf81aeb95e566",
+    "url": "/Portfolio-1.0/static/media/Eleven22.938d449c.png"
+  },
+  {
+    "revision": "2331a7c9a0b0936418622607838bd177",
+    "url": "/Portfolio-1.0/static/media/GestureLife.2331a7c9.png"
+  },
+  {
+    "revision": "67d9eb7df2b3eb8b566d1f7a1d9c60ca",
+    "url": "/Portfolio-1.0/static/media/ManhattahnB&W.67d9eb7d.png"
+  },
+  {
+    "revision": "d8ef0589f73bf9dde2c9ad1228d7012f",
+    "url": "/Portfolio-1.0/static/media/RestfulAPI.d8ef0589.png"
+  },
+  {
+    "revision": "44b16efa12b6dd3c81aed15aa9dd18dc",
+    "url": "/Portfolio-1.0/static/media/angular.44b16efa.png"
+  },
+  {
+    "revision": "193312512e5756a46a6b002abcc127d2",
+    "url": "/Portfolio-1.0/static/media/computerDraw.19331251.png"
+  },
+  {
+    "revision": "77dfd6d640a9e298a200d138921e04f6",
+    "url": "/Portfolio-1.0/static/media/f23-dev.77dfd6d6.png"
+  },
+  {
+    "revision": "9a20cdcb477bb05ceecbc4eb53694787",
+    "url": "/Portfolio-1.0/static/media/f23.9a20cdcb.png"
+  },
+  {
+    "revision": "4c335fedc767b0f5fd204b89c82ca5de",
+    "url": "/Portfolio-1.0/static/media/figma.4c335fed.png"
+  },
+  {
+    "revision": "31e089f741184f3aaddede9cdeca636e",
+    "url": "/Portfolio-1.0/static/media/sp.31e089f7.png"
+  }
+]);
